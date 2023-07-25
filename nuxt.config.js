d15fcc8c37babaa84ac64c39f0c085d259d68a49
ryan <ryan@arrayofstars.com>
2023-07-25 14:27:50 -0400

    project rename

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'AOS - Portals',
+    title: 'AOS - AR Gallery Demo',
     htmlAttrs: {
       lang: 'en',
     },
